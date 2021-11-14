92abbc333ed63950a5bf30ef7ba8e826ec44bcfd
Your Name <you@example.com>
2021-11-14 19:57:23 +0330

    added reducer to project

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,8 +4,7 @@ import { useProducts, useProductsActions } from "../Providers/ProductsProvider";
 
 const ProductList = (props) => {
   const products = useProducts();
-  const { removeHandler, incrementHandler, decrementHandler, changeHandler } =
-    useProductsActions();
+  const dispatch = useProductsActions();
   if (products.length === 0) {
     return <div>There is no product in cart</div>;
   }
@@ -15,10 +14,10 @@ const ProductList = (props) => {
         return (
           <Product
             product={product}
-            click={() => removeHandler(product.id)}
-            onIncrement={() => incrementHandler(product.id)}
-            onDecrement={() => decrementHandler(product.id)}
-            change={(e) => changeHandler(e, product.id)}
+            click={() => dispatch({ type: "remove", id: product.id })}
+            onIncrement={() => dispatch({ type: "increment", id: product.id })}
+            onDecrement={() => dispatch({ type: "decrement", id: product.id })}
+            change={(e) => dispatch({ type: "edit", id: product.id, event: e })}
           />
         );
       })}
--- src/components/Providers/ProductsProvider.js
+++ src/components/Providers/ProductsProvider.js
@@ -1,18 +1,64 @@
-import React, { useState, useContext } from "react";
+import React, { useContext, useReducer } from "react";
 
 const ProductContext = React.createContext();
 const ProductContextDispatcher = React.createContext();
+const initialState = [
+  { name: "React.js", price: "99 $", id: 1, quantity: 1 },
+  { name: "Node.js", price: "69 $", id: 2, quantity: 2 },
+  { name: "Veu.js", price: "59 $", id: 3, quantity: 3 },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment": {
+      const index = state.findIndex((item) => item.id === action.id);
+      const product = { ...state[index] };
+      product.quantity++;
+      const UpdatedProducts = [...state];
+      UpdatedProducts[index] = product;
+
+      return UpdatedProducts;
+    }
+
+    case "decrement": {
+      const index = state.findIndex((item) => item.id === action.id);
+      const product = { ...state[index] };
+
+      if (product.quantity === 1) {
+        const filteredItem = state.filter((p) => p.id !== action.id);
+        return filteredItem;
+      } else {
+        const UpdatedProducts = [...state];
+        product.quantity--;
+        UpdatedProducts[index] = product;
+        return UpdatedProducts;
+      }
+    }
+
+    case "edit": {
+      const index = state.findIndex((item) => item.id === action.id);
+      const product = { ...state[index] };
+      // 3- clone the selected index and update the value
+      product.name = action.event.target.value;
+      const UpdatedProducts = [...state];
+      UpdatedProducts[index] = product;
+      return UpdatedProducts;
+    }
+    case "remove":
+      const filteredProducts = state.filter((p) => p.id !== action.id);
+      return filteredProducts;
+
+    default:
+      return state;
+  }
+};
 
 const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([
-    { name: "React.js", price: "99 $", id: 1, quantity: 1 },
-    { name: "Node.js", price: "69 $", id: 2, quantity: 2 },
-    { name: "Veu.js", price: "59 $", id: 3, quantity: 3 },
-  ]);
+  const [products, dispatch] = useReducer(reducer, initialState);
 
   return (
     <ProductContext.Provider value={products}>
-      <ProductContextDispatcher.Provider value={setProducts}>
+      <ProductContextDispatcher.Provider value={dispatch}>
         {children}
       </ProductContextDispatcher.Provider>
     </ProductContext.Provider>
@@ -23,46 +69,5 @@ export default ProductsProvider;
 
 export const useProducts = () => useContext(ProductContext);
 export const useProductsActions = () => {
-  const setProducts = useContext(ProductContextDispatcher);
-  const products = useContext(ProductContext);
-  const removeHandler = (id) => {
-    const filteredProducts = products.filter((p) => p.id !== id);
-    setProducts(filteredProducts);
-  };
-  const incrementHandler = (id) => {
-    // 1- id==> ok
-    // 2- index
-    const index = products.findIndex((item) => item.id === id);
-    const product = { ...products[index] };
-    // 3- clone the selected index and update the quantity
-    product.quantity++;
-    const UpdatedProducts = [...products];
-    UpdatedProducts[index] = product;
-    setProducts(UpdatedProducts);
-  };
-  const decrementHandler = (id) => {
-    const index = products.findIndex((item) => item.id === id);
-    const product = { ...products[index] };
-
-    if (product.quantity === 1) {
-      const filteredItem = products.filter((p) => p.id !== id);
-      setProducts(filteredItem);
-    } else {
-      const UpdatedProducts = [...products];
-      product.quantity--;
-      UpdatedProducts[index] = product;
-      setProducts(UpdatedProducts);
-    }
-  };
-
-  const changeHandler = (e, id) => {
-    const index = products.findIndex((item) => item.id === id);
-    const product = { ...products[index] };
-    // 3- clone the selected index and update the value
-    product.name = e.target.value;
-    const UpdatedProducts = [...products];
-    UpdatedProducts[index] = product;
-    setProducts(UpdatedProducts);
-  };
-  return { removeHandler, incrementHandler, decrementHandler, changeHandler };
+  return useContext(ProductContextDispatcher);
 };
